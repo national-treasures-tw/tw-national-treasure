d963bf40751fcf89146f06aff6963cbc9b1351f7
Hsin <shinsonh@gmail.com>
2016-12-14 17:46:24 -0500

    change to save on local json

--- saveNara.js
+++ saveNara.js
@@ -1,34 +1,31 @@
-var firebase = require('firebase');
-firebase.initializeApp({databaseURL: "https://national-treasure.firebaseio.com"});
-
-var db = firebase.database();
-var ref = db.ref("/nara");
-var apiRef = ref.child("api");
-
 var axios = require('axios');
 
-// some record has '$' in 'publicContributions' that causes Firebase to crash
-// so this helper strips them out
-const stripDollarSign = (objectArray) => {
-	return objectArray.map(object => {
-		if (object.publicContributions && object.publicContributions.tags && object.publicContributions.tags.tag.length > 0) {
-			return Object.assign(object, {}, {
-				publicContributions: {}
-			});
-		} else {
-			return object;
-		}
-	});
-}
+var fs = require('fs');
 
-axios.get('https://catalog.archives.gov/api/v1/?q=taiwan&rows=100')
-.then(response => {
-	apiRef.set(stripDollarSign(response.data.opaResponse.results.result), error => {
-		if (error) {
-			console.log(error);
-		} else {
-			console.log('done');
-		}
-	});
-})
-.catch(error => console.log(error));
+setInterval(() => {
+	let json = require('./nara.json');
+	let total = json.results.total;
+	let currentNumber = json.results.result.length;
+	let rowNumber = (total - currentNumber > 1000) ? 1000 : total - currentNumber
+
+	axios.get(`https://catalog.archives.gov/api/v1/?q=taiwan&offset=${currentNumber}&rows=1000`)
+	.then(response => {
+		const newResultsArray = response.data.opaResponse.results.result;
+		const jsonArray = Object.assign(json, {
+			results: Object.assign(json.results, {
+				result: [...json.results.result, ...newResultsArray]
+			})
+		});
+		var outputFilename = 'nara.json';
+
+		fs.writeFile(outputFilename, JSON.stringify(jsonArray), function(err) {
+		    if(err) {
+		      console.log(err);
+		    } else {
+		      console.log("JSON saved to " + outputFilename + ' with ' + currentNumber + ' results ');
+		    }
+		});
+	})
+	.catch(error => console.log(error));
+
+}, 15000)
