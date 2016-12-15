be055f686ada6089fe8226c63ae32c7b338cd622
Hsin <shinsonh@gmail.com>
2016-12-15 16:59:12 -0500

    adding college park specific text records

---
+++ scripts/parseHTML.js
@@ -0,0 +1,34 @@
+var jsdom = require("jsdom");
+var _ = require('lodash');
+var fs = require('fs');
+
+jsdom.env({
+    file: './data/search-3.htm',
+    done: function (err, window) {
+        global.window = window;
+        global.document = window.document;
+        // now you can work on parsing HTML as you normally would in a browser
+        // e.g. this will work  
+        scrape();
+    }
+});
+function scrape() {
+    var anchors = document.getElementsByTagName('a');
+    // console.log(anchors[500].href)
+    var arr = [].slice.call(anchors);
+    var mapped = arr.map(a => a.href.split('/')[4] ? a.href.split('/')[4].split('?')[0] : null)
+    .map(el => (el && !!parseInt(el)) ? el : null );
+
+    var naIds = _.compact(mapped);
+    var naIdJSON = naIds.map(id => ( { naId: id }));
+
+    var outputFilename = 'nara-tw-college-park-web-3.json';
+        
+    fs.writeFile(outputFilename, JSON.stringify(naIdJSON), function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("JSON saved to " + outputFilename);
+        }
+    })
+}
